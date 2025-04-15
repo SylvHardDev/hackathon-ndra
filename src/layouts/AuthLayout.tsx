deef38acf432c31dcb53da5965e0294f815da1d7
Mihajatiana <miandriakotonerason@gmail.com>
2025-04-15 21:39:44 +0300

    add: setup user status listener

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,15 @@
-import { Outlet } from "react-router-dom";
+import useAuth from "@/hooks/useAuth";
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
+  const { userStatus } = useAuth();
+  const accessToken = localStorage.getItem("token");
+  const refreshToken = localStorage.getItem("refresh_token");
+
+  if (userStatus === "signed-out" && (!accessToken || !refreshToken)) {
+    return <Navigate to="/" replace />;
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="w-full max-w-md">
